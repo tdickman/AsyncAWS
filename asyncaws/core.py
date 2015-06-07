5c6de9b4c9565c49443e3ccd79705ca34b14f1f5
Tom Dickman <tdickman@rmn.com>
2015-06-07 16:47:28 -0500

    Proper utf-8 encoding for python3

--- asyncaws/core.py
+++ asyncaws/core.py
@@ -53,7 +53,7 @@ class AWSRequest(HTTPRequest):
             host=host, amz_date=amz_date)
         signed_headers = 'host;x-amz-date'
         # for GET requests payload is empty
-        payload_hash = hashlib.sha256('').hexdigest()
+        payload_hash = hashlib.sha256(''.encode('utf-8')).hexdigest()
 
         canonical_request = (
             '{method}\n{canonical_uri}\n{canonical_querystring}'
@@ -70,7 +70,7 @@ class AWSRequest(HTTPRequest):
             date_stamp=date_stamp, region=region, service=service)
         string_to_sign = '{algorithm}\n{amz_date}\n{scope}\n{hash}'.format(
             algorithm=algorithm, amz_date=amz_date, scope=scope,
-            hash=hashlib.sha256(canonical_request).hexdigest())
+            hash=hashlib.sha256(canonical_request.encode('utf-8')).hexdigest())
         sign_key = get_signature_key(kwargs['secret_key'],
                                      date_stamp, region, service)
         hash_tuple = (sign_key, string_to_sign.encode('utf-8'), hashlib.sha256)
